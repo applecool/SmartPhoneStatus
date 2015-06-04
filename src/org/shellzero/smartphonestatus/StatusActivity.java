e5417079218bdb57d5eb3a78b72e93a89a15af2e
applecool <kmitharsha@gmail.com>
2015-06-03 23:01:34 -0400

    Fixed formatting

--- src/org/shellzero/smartphonestatus/StatusActivity.java
+++ src/org/shellzero/smartphonestatus/StatusActivity.java
@@ -11,33 +11,35 @@ import org.shellzero.smartphonestatus.R;
 @SuppressWarnings("deprecation")
 public class StatusActivity extends TabActivity {
 
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        TabHost host = getTabHost();
-        
-        TabSpec statusspec = host.newTabSpec("Phone");
-        statusspec.setIndicator("Phone",getResources().getDrawable(R.drawable.nphone));
-        Intent phoneStatusIntent = new Intent(this, PhoneStatusActivity.class);
-        statusspec.setContent(phoneStatusIntent);
-        
-        TabSpec batteryspec = host.newTabSpec("Battery");
-        batteryspec.setIndicator("Battery", getResources().getDrawable(R.drawable.nbattery));
-        Intent batteryIntent = new Intent(this, BatteryStatusActivity.class);
-        batteryspec.setContent(batteryIntent);
-        
-        // Adding all TabSpec to TabHost
-        host.addTab(statusspec); 
-        host.addTab(batteryspec);
-        
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.phonestatus, menu);
-        return true;
-    }
+		TabHost host = getTabHost();
+
+		TabSpec statusspec = host.newTabSpec("Phone");
+		statusspec.setIndicator("Phone",
+				getResources().getDrawable(R.drawable.nphone));
+		Intent phoneStatusIntent = new Intent(this, PhoneStatusActivity.class);
+		statusspec.setContent(phoneStatusIntent);
+
+		TabSpec batteryspec = host.newTabSpec("Battery");
+		batteryspec.setIndicator("Battery",
+				getResources().getDrawable(R.drawable.nbattery));
+		Intent batteryIntent = new Intent(this, BatteryStatusActivity.class);
+		batteryspec.setContent(batteryIntent);
+
+		// Adding all TabSpec to TabHost
+		host.addTab(statusspec);
+		host.addTab(batteryspec);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.phonestatus, menu);
+		return true;
+	}
 }
